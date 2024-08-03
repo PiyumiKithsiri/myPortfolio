6c19aa50ccb0b585b7f84cdd16dfb3200d3c824e
piyumi <piyumirandi6@gmail.com>
2024-08-03 17:29:27 +0530

    navbar setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button";
 
 //components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -27,7 +28,7 @@ const Header = () => {
 
           {/*mobile nav*/}
           <div className="xl:hidden">
-            mobile nav
+            <MobileNav />
           </div>
         
       </div>
--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client";
 
-const PageTransition = () => {
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+
+const PageTransition = ({children}) => {
+  const pathname = usePathname();
   return (
     <div>
-      PageTransition
+      <AnimatePresence>
+        <div key={pathname}>
+          <motion.div 
+            initial={{opacity:1}} 
+            animate={{
+              opacity:0, 
+              transition:{delay: 1, duration: 0.4, ease:'easeInOut'},
+              }}
+              className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+          />
+          {children}
+        </div>
+      </AnimatePresence>
     </div>
   )
 }
 
-export default PageTransition
+export default PageTransition;
